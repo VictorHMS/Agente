8853c4bc21a13249986f2f0ed38677867e76979c
VictorHMS <39633965+VictorHMS@users.noreply.github.com>
2022-03-07 03:03:52 -0300

    teste dfs

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -6,14 +6,27 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour
 {
     public GameControler controle;
-    List<Tuple<int,int,int>> getAdjCells(int lin, int col)
+    protected bool[,] visited;
+    public bool terminei;
+    public List<Tuple<int,int,int>> getAdjCells(int lin, int col)
     {
+        controle = gameObject.GetComponent<GameControler>();
         List<Tuple<int, int, int>> adj = new List<Tuple<int, int, int>>();
         if (controle.exist(lin + 1, col)) adj.Add(new Tuple<int, int, int>(controle.getPeso(lin + 1, col), lin + 1, col));
         if (controle.exist(lin - 1, col)) adj.Add(new Tuple<int, int, int>(controle.getPeso(lin - 1, col), lin - 1, col));
         if (controle.exist(lin, col + 1)) adj.Add(new Tuple<int, int, int>(controle.getPeso(lin, col + 1), lin, col + 1));
         if (controle.exist(lin, col - 1)) adj.Add(new Tuple<int, int, int>(controle.getPeso(lin, col - 1), lin, col - 1));
         return adj;
+    }
+
+    public virtual void init(Tuple<int, int> inicial)
+    {
+
+    }
+
+    public virtual Tuple<int, int> iteration(Tuple<int,int> destino)
+    {
+        return new Tuple<int, int>(0, 0);
     }
     // Start is called before the first frame update
     void Start()
