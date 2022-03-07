ebc1cbe476ef44051e880c395cc6be4f96b39bbc
VictorHMS <39633965+VictorHMS@users.noreply.github.com>
2022-03-07 03:03:33 -0300

    teste dfs

---
+++ Assets/Scripts/Dfs.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Dfs : PathFinder
+{
+    Stack<Tuple<int, int, int>> pilha = new Stack<Tuple<int, int, int>>(); //na tupla tem a posição a ser analizada e o ultimo índice analisado para poder continuar a iteração
+    
+    public override void init(Tuple<int, int> inicial) //inicial
+    {
+        controle = gameObject.GetComponent<GameControler>();
+        int i = inicial.Item1;
+        int j = inicial.Item2;
+        Debug.Log("deu a");
+        visited = new bool[controle.getNRows(), controle.getNCols()];
+        pilha.Push(new Tuple<int, int, int>(0, i, j));
+        terminei = false;
+        
+    }
+
+    public override Tuple<int, int> iteration(Tuple<int, int> destino)
+    {
+
+        Tuple<int, int, int> aux = new Tuple<int, int, int>(-1, -1, -1);
+        Tuple<int, int> next = new Tuple<int, int>(-1, -1);
+        Tuple<int, int, int> node = new Tuple<int, int, int>(-1, -1, -1);
+        List<Tuple<int, int, int>> adj = new List<Tuple<int, int, int>>();
+        while (pilha.Count != 0)
+        {
+            aux = pilha.Pop();
+            adj = getAdjCells(aux.Item2, aux.Item3);
+            if (adj.Count > aux.Item1)
+            {
+                int val = 0;
+                bool deu = false;
+                while (val+aux.Item1 < adj.Count)
+                {
+                    node = adj[aux.Item1 + val];
+                    if (!visited[node.Item2, node.Item3])
+                    {
+                        deu = true;
+                        break;
+                    }
+                    val++;
+                }
+                //debug:
+                if (aux.Item2 == -1) Debug.Log("Nao atualizou");
+
+                pilha.Push(new Tuple<int, int, int>(aux.Item1 + val + 1, aux.Item2, aux.Item3));
+
+                if (deu)
+                {
+                    visited[node.Item2, node.Item3] = true;
+                    next = new Tuple<int, int>(node.Item2, node.Item3);
+                    pilha.Push(new Tuple<int, int, int>(0, node.Item2, node.Item3));
+
+                    if (next.Item1 == destino.Item1 && next.Item2 == destino.Item2) terminei = true;
+                    //debug:
+                    if (node.Item2 == -1) Debug.Log("Nao atualizou o 2");
+
+                    return next;
+                }
+
+            }
+        }
+
+        terminei = true;
+
+        return next;
+
+    }
+}
--- Assets/Scripts/GameControler.cs
+++ Assets/Scripts/GameControler.cs
@@ -9,7 +9,7 @@ public class GameControler : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[,] tabuleiro { get; set; }
     public int[,] campo;
-    public int[] weight = {1, 5, 3, INF};
+    public int[] weight = { 1, 5, 3, INF };
     public GameObject field;
     public GameObject tile;
     public Text textinho;
@@ -19,11 +19,26 @@ public class GameControler : MonoBehaviour
     public const int INF = 1000000000;
     public Vector3 posFruta = new Vector3();
     public Vector3 posAgente = new Vector3();
+    public Tuple<int, int> indAgente, indFruta;
+    private int state = 0;
+    Dfs dfs;
+    Tuple<int, int> node;
+    float tempo = 0;
     enum buscasType
     {
         Largura, Profundidade, CustoUniforme, Gulosa, AStar
     };
      //cons = Enum.Parse(typeof(buscasType), textinho.toString);
+
+    public int getNRows()
+    {
+        return 40;
+    }
+
+    public int getNCols()
+    {
+        return 60;
+    }
 
     void randAgente()
     {
@@ -36,6 +51,7 @@ public class GameControler : MonoBehaviour
         } while (getPeso(indI, indJ) == INF);
 
         posAgente = (tabuleiro[indI, indJ].transform.position);
+        indAgente = new Tuple<int, int>(indI,indJ);
         Debug.Log(indI);
         Debug.Log(indJ);
     }
@@ -51,6 +67,7 @@ public class GameControler : MonoBehaviour
         } while (getPeso(indI, indJ) == INF);
 
         posFruta = tabuleiro[indI, indJ].transform.position;
+        indFruta = new Tuple<int, int>(indI, indJ);
     }
 
     void generateMap()
@@ -83,6 +100,8 @@ public class GameControler : MonoBehaviour
         }
         randAgente();
         randfruta();
+        node = new Tuple<int, int>(indAgente.Item1, indAgente.Item2);
+        tempo = 0;
     }
 
     public int getPeso(int i, int j)
@@ -92,8 +111,8 @@ public class GameControler : MonoBehaviour
 
     public bool exist(int i, int j)
     {
-        if (i > NROWS || i < 0) return false;
-        if (j > NCOLS || j < 0) return false;
+        if (i >= NROWS || i < 0) return false;
+        if (j >= NCOLS || j < 0) return false;
         if (campo[i, j] == 3) return false;
         return true;
     }
@@ -112,6 +131,7 @@ public class GameControler : MonoBehaviour
 
     void Start()
     {
+        dfs = gameObject.AddComponent<Dfs>();
         field = GameObject.Find("Field");
         tabuleiro = new GameObject[NROWS, NCOLS];
         campo = new int[NROWS, NCOLS];
@@ -144,33 +164,77 @@ public class GameControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        buscasType type = (buscasType) Enum.Parse(typeof(buscasType), textinho.text);
-        List<Vector3>path = new List<Vector3>();
-        switch(type) {
-            case buscasType.Largura:
-                //TODO: algotitmo
-                path.Add(posFruta); //temporariamente so vai para a fruto sem o path
-                break;
-            case buscasType.Profundidade:
-                //TODO: algotitmo
-                break;
-            case buscasType.CustoUniforme:
-                //TODO: algotitmo
-                break;
-            case buscasType.Gulosa:
-                //TODO: algotitmo
-                break;
-            case buscasType.AStar:
-                //TODO: algotitmo
-                break;
+        if (state == 5)
+        {
+            buscasType type = (buscasType)Enum.Parse(typeof(buscasType), textinho.text);
+            List<Vector3> path = new List<Vector3>();
+            switch (type)
+            {
+                case buscasType.Largura:
+                    //TODO: algotitmo
+                    path.Add(posFruta); //temporariamente so vai para a fruto sem o path
+                    break;
+                case buscasType.Profundidade:
+                    //TODO: algotitmo
+                    break;
+                case buscasType.CustoUniforme:
+                    //TODO: algotitmo
+                    break;
+                case buscasType.Gulosa:
+                    //TODO: algotitmo
+                    break;
+                case buscasType.AStar:
+                    //TODO: algotitmo
+                    break;
+            }
+
+            if (path.Count > 0)
+            {
+                //achei um caminho
+                GotoPoint(path[0]);
+
+                updateFruta();
+            }
+        }
+        else if (state == 0)
+        {
+            generateMap();
+            state = 1;
+        }
+        else if(state == 1)
+        {
+            dfs.init(indAgente);
+            state = 2;
+        }else if(state == 2)
+        {
+            float nt = Time.deltaTime;
+            tempo += nt;
+            if(tempo >= 0.1)
+            {
+                if (exist(node.Item1, node.Item2)) esmaecer(node);
+                if (dfs.terminei) state = 0;
+                else
+                {
+                    node = dfs.iteration(indFruta);
+                    if(exist(node.Item1,node.Item2))pintar(node);
+                }
+                tempo = 0;
+            }
+            
         }
 
-        if (path.Count > 0) {
-            //achei um caminho
-            GotoPoint(path[0]);
+    }
 
-            updateFruta();
-        }
+    void pintar(Tuple<int,int> posi)
+    {
+        var aux = tabuleiro[posi.Item1,posi.Item2].GetComponent<Renderer>();
+        aux.material.SetColor("_Color", Color.black);
+    }
+
+    void esmaecer(Tuple<int, int> posi)
+    {
+        var aux = tabuleiro[posi.Item1, posi.Item2].GetComponent<Renderer>();
+        aux.material.SetColor("_Color", Color.cyan);
     }
 
 }
